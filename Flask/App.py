f056e0ba78c24cbcd4495b97c4a2cdd1f8ed54ed
JSEC <juansec13@gmail.com>
2020-08-06 16:32:06 -0500

    BASE BACKEND PARA PROYECTO

--- Flask/App.py
+++ Flask/App.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 #Mysql Connection
 app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='1234'
-app.config['MYSQL_DB']='test'
+app.config['MYSQL_USER']='usuario'
+app.config['MYSQL_PASSWORD']='contrasena'
+app.config['MYSQL_DB']='liga_de_campeones'
 mysql = MySQL(app)
 
 #settings
