b46eab05a22b32d778f9cb3a9667efa04222177a
JSEC <juansec13@gmail.com>
2020-08-30 21:28:10 -0500

    Logo prueba

--- Flask/App.py
+++ Flask/App.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 #Mysql Connection
 app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='usuario'
+app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']='1234'
 app.config['MYSQL_DB']='proyecto'
 mysql = MySQL(app)
@@ -477,4 +477,4 @@ def ins_curEsp(ide,idc):
 
 
 if __name__ == '__main__':
-    app.run(port=3400, debug=True)
+    app.run(port=3100, debug=True)
